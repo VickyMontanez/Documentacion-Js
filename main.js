32699057dc039ebd00c9cfdd6cd666f570e0e376
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 15:34:06 -0500

    feat: :sparkles: Add a Slice Method of the Strings
    The method '.slice' divide a section of the string and create a new string with that piece

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 let parrafo="A Cuesta le cuesta subir la cuesta, y en medio de la cuesta, va y se acuesta";
 document.querySelector('body').textContent=parrafo
 console.log(parrafo)
-//5.Método replaceAll
-console.log(parrafo.replaceAll("cuesta", "pato"));
+//6.Método slice
+console.log(parrafo.slice(0,8));
 
