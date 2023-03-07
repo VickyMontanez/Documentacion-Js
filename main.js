039630f3dc07f3830ad4a66c51693fc53e90323c
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 09:24:14 -0500

    feat: :sparkles: Add the first method of strings JS
    The firs method of strings from JavaScript is '.length', this help me to count the charactheres of the principal string.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-let parrafo=""
+//METODOS DE STRINGS
+let parrafo="Un hamburguesa huernafa, es una hamburguesa sin papas :(";
+document.querySelector('body').textContent=parrafo
+console.log(parrafo
+    )
+//1.  Método Length
+console.log(`El primer método '.length' me muestra los caracteres de mi cadena principal son: `+ parrafo.length)
+
