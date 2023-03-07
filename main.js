daec56583c9ba1c0ccd62ae38d2d7852dc9e56fb
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 16:17:45 -0500

    feat: :sparkles: Add a CodePointAt Method of the Strings
    The method '.codePointAt' post the unicode of the letter using the index of the string

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ document.querySelector('body').textContent=parrafo;
 console.log(parrafo);
 let index="Hola";
 console.log(index)
-//11.Método charCodetAt
+//12.Método codePointAT
 console.log(`Este es el largo de mi string `+ parrafo.length)
-console.log(`Este es el unicode de la letra'C' `+ parrafo.charCodeAt(3));
-console.log(`Este es el unicode de la letra'H' `+ index.charCodeAt(0));
-console.log(`Este es el unicode de la letra'a' `+ index.charCodeAt(3));
+console.log(`Este es el unicode de la letra'C' `+ parrafo.codePointAt(3));
+console.log(`Este es el unicode de la letra'H' `+ index.codePointAt(0));
+console.log(`Este es el unicode de la letra'a' `+ index.codePointAt(3));
 
 
