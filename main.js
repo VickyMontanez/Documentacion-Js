5ae6976acb9ac8d8265f374c994b0d13b4ad7554
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 15:55:04 -0500

    feat: :sparkles: Add a CharAt Method of th Strings
    The method '.charAt' return a position from the index of the principal string

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 let parrafo="A Cuesta le cuesta subir la cuesta, y en medio de la cuesta, va y se acuesta";
 document.querySelector('body').textContent=parrafo
 console.log(parrafo)
-//9.Método split
-console.log(parrafo.split(" "));
-console.log(parrafo.split("a"));
+//9.Método chartAt
+console.log(parrafo.charAt(38));
+console.log(parrafo.charAt(4));
+console.log(parrafo.charAt(9));
+
 
