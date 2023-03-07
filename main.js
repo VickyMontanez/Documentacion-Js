2b32acb0f872782814f93967713c8e92cba682a2
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 15:38:26 -0500

    feat: :sparkles: Add a Substring Method of the Strings
    The method '.substring' substract a fragment of the principal string.

--- main.js
+++ main.js
@@ -2,6 +2,6 @@
 let parrafo="A Cuesta le cuesta subir la cuesta, y en medio de la cuesta, va y se acuesta";
 document.querySelector('body').textContent=parrafo
 console.log(parrafo)
-//6.Método slice
-console.log(parrafo.slice(0,8));
+//7.Método substring
+console.log(parrafo.substring(9,34));
 
