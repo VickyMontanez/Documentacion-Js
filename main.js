04bb69fcd7afeafa4b23e7590c23f7993be0d329
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-07 16:11:32 -0500

    feat: :sparkles: Add a CharCodeAt Method of the Strings
    The method '.charCodeAt' post the unicode of the letters of the string

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 //METODOS DE STRINGS
 let parrafo="A Cuesta le cuesta subir la cuesta, y en medio de la cuesta, va y se acuesta";
-document.querySelector('body').textContent=parrafo
-console.log(parrafo)
-//9.Método chartAt
-console.log(parrafo.charAt(38));
-console.log(parrafo.charAt(4));
-console.log(parrafo.charAt(9));
+document.querySelector('body').textContent=parrafo;
+console.log(parrafo);
+let index="Hola";
+console.log(index)
+//11.Método charCodetAt
+console.log(`Este es el largo de mi string `+ parrafo.length)
+console.log(`Este es el unicode de la letra'C' `+ parrafo.charCodeAt(3));
+console.log(`Este es el unicode de la letra'H' `+ index.charCodeAt(0));
+console.log(`Este es el unicode de la letra'a' `+ index.charCodeAt(3));
 
 
