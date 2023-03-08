078a85ee214fb4ebd9722e97359750083fdf65bc
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-08 15:21:00 -0500

    feat: :sparkles: Add a MatchAll Method of the Strings
    The method '.matchAll' return  an iterator of all results matching a string for the RegExp

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 //METODOS DE STRINGS
-let str1="ABCDEJGHIabcdefghji";
-let str2="Edificio Zona franca";
-let str3="GATICOS EN aDOPCIÓN";
 
-//20. Método match
-let regexp=/[A-C]/gi;
-let match = str1.match(regexp);
-let regexp2=/[A-Z]/g;
-let match2= str2.match(regexp2);
-let regexp3=/([a-z])/g;
-let match3= str3.match(regexp3);
+//21 Método matchAll
 
-console.log(match);
-console.log(match2);
-console.log(match3);
+const regexp = RegExp('foo[a-z]*','g');
+const cadena = 'mesa football, football';
+const ocurrencias = cadena.matchAll(regexp);
+console.log(Array.from(cadena.matchAll(regexp), m => m[0]));
