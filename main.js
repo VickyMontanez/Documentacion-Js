7cc983d9a791c4a805e76b82b6514a7408caedc6
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-08 06:38:30 -0500

    feat: :sparkles: Add a FromCharCode Method of the Strings
    The method '.fromCharCode' It allows create a new string with the Unicode Char of the letters

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 //METODOS DE STRINGS
-let parrafo="A Cuesta le cuesta subir la cuesta, y en medio de la cuesta, va y se acuesta";
+let parrafo="And it's the thousandth time that's it's even bolder, Don't be surprised when you get bent over, They told ya, but you were dying for it";
 document.querySelector('body').textContent=parrafo;
 console.log(parrafo);
-let index="Hola";
-console.log(index)
-//12.Método codePointAT
-console.log(`Este es el largo de mi string `+ parrafo.length)
-console.log(`Este es el unicode de la letra'C' `+ parrafo.codePointAt(3));
-console.log(`Este es el unicode de la letra'H' `+ index.codePointAt(0));
-console.log(`Este es el unicode de la letra'a' `+ index.codePointAt(3));
+let index="ABC";
+console.log(`Esta es una cadena creada por un 'let' `+index)
+//13.Método fromCharCode
+console.log(String.fromCharCode(65,66,67)+` Esta es una cadena creada por el método '.fromCharCode`)
+console.log(String.fromCharCode(67,65,77,80,85,83))
+console.log(String.fromCharCode(83,80,85,84,78,73,75))
+
+
 
 
