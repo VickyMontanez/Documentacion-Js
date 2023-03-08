615a9219ea3cb0b84d32274ea70905a62f1b754a
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-08 08:52:14 -0500

    feat: :sparkles: Add a IndexOf Method of the Strings
    The method '.indexOf' it allows to locate a letter or word, counting by an index.

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 //METODOS DE STRINGS
-let parrafo="Arabella's got some interstellar-gator skin boots";
-document.querySelector('body').textContent=parrafo;
-console.log(parrafo);
 let index="And a Helter Skelter 'round her little finger and I ride it endlessly";
-
-//14. Método concat
-let str1="primer string";
-let str2="segundo string"
-console.log(parrafo.concat(" "+ index))
-console.log(str1.concat(" se puede concatenar con "+ str2))
+let string="Arrabella";
+let ejemplo="Ballena Azul"
+//15. Método indexOf
+console.log(ejemplo);
+console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 0 en la palabra "Ballena" = `+(ejemplo.indexOf("a",0)));
+console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 2 en la palabra "Ballena" = `+ejemplo.indexOf("a",2));
+console.log(string);
+console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 0 en la palabra "Arrabella" = `+(string.indexOf("a",0)));
+console.log(`Este es el indice de la letra "A", empezando a contar desde el indice 0 en la palabra "Arrabella" = `+(string.indexOf("A",0)));
