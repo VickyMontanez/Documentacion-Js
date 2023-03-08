eeb81f1664a0d4adff9bf4c353d430b2ca29eb87
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-08 09:00:19 -0500

    feat: :sparkles: Add a LastIndexOf Method of the Strings
    The method '.lastIndexOf' it allows find  the last index of  letter or word

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 //METODOS DE STRINGS
 let index="And a Helter Skelter 'round her little finger and I ride it endlessly";
-let string="Arrabella";
 let ejemplo="Ballena Azul"
-//15. Método indexOf
+//16. Método lastIndexOf
 console.log(ejemplo);
-console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 0 en la palabra "Ballena" = `+(ejemplo.indexOf("a",0)));
-console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 2 en la palabra "Ballena" = `+ejemplo.indexOf("a",2));
-console.log(string);
-console.log(`Este es el indice de la letra "a", empezando a contar desde el indice 0 en la palabra "Arrabella" = `+(string.indexOf("a",0)));
-console.log(`Este es el indice de la letra "A", empezando a contar desde el indice 0 en la palabra "Arrabella" = `+(string.indexOf("A",0)));
+console.log(`Este es el indice de la letra "l" con '.indexOf' = `+(ejemplo.indexOf("l")));
+console.log(`Este es el indice de la última letra "l" con '.lastIndexOf' = `+ejemplo.lastIndexOf("l"));
