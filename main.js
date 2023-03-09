b05b503929002502dcafb6e3f8d571cb72641bf0
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-09 08:12:43 -0500

    feat: :sparkles: Add Logic Operations of JavaScript
    The Logic operations allow working with basic operations in a binary level

--- main.js
+++ main.js
@@ -1,69 +1,33 @@
-//OPERADORES DE COMPARACIÓN
-
-//1.OPERADOR DE IGUALDAD
-let a = 5;
-let b = 5;
-let c= 6;
-console.log("1.OPERADOR DE IGUALDAD");
-console.log('a = 5');
-console.log('b = 5');
-console.log("a == b : ", a==b);
-console.log('a = 5');
-console.log('c = 6');
-console.log("a == c : ", a==c);
-//2.OPERADOR DE DESIGUALDAD
-console.log("2.OPERADOR DE DESIGUALDAD");
-let d = "gatos";
-let e = 65;
-console.log('a = 5');
-console.log('b = 5');
-console.log("a != b : ", a!=b);
-console.log('d = "gatos"');
-console.log('e = 65');
-console.log("d != e : ", d!=e);
-//3.OPERADOR MAYOR QUE
-console.log("3.OPERADOR MAYOR QUE");
-let f = 16;
-let g = 5;
-console.log("f = 16");
-console.log("g = 5");
-console.log("f > g = ",f>g);
-console.log("g > f = ",g>f);
-//4.OPERADOR MAYOR O IGUAL QUE
-console.log("4.OPERADOR MAYOR QUE O IGUAL QUE");
-let h = 16;
-let i = 16;
-console.log("h = 16");
-console.log("i = 16");
-console.log("h >= i = ",h>=i);
-console.log("h = 16");
-console.log("g = 5");
-console.log("g >= h = ",g>=h);
-//5.OPERADOR MENOR QUE
-console.log("5.OPERADOR MENOR QUE");
-console.log("f = 16");
-console.log("g = 5");
-console.log("g < f = ",g<f);
-console.log("f < g = ",f<g);
-//6.OPERADOR MENOR O IGUAL QUE
-console.log("6.OPERADOR MENOR QUE O IGUAL QUE");
-console.log("h = 16");
-console.log("g = 5");
-console.log("g <= h = ",g<=h);
-console.log("h <= g = ",h<=g);
-//7.OPERADOR DE IDENTIDAD
-console.log("7.OPERADOR DE IDENTIDAD");
-console.log('a = 5');
-console.log('b = 5');
-console.log("a === b : ", a===b);
-console.log('a = 5');
-console.log('c = 6');
-console.log("a === c : ", a===c);
-//8.OPERADOR NO IDENTICO
-console.log("8.OPERADOR NO IDENTICO");
-console.log('d = "gatos"');
-console.log('e = 65');
-console.log("d !== e : ", d!==e);
-console.log('a = 5');
-console.log('b = 5');
-console.log("a !== b : ", a!==b);
+//OPERADORES LOGICOS
+//1.OPERADOR AND
+console.log("1.OPERADOR AND");
+let a = 1;
+let b = 1;
+let c = 0;
+let d = 0;
+console.log("a = 1");
+console.log("b = 1");
+console.log("c = 0");
+console.log("a & b = ", a & b);
+console.log("a & c = ", a & c);
+//2.OPERADOR OR
+console.log("2.OPERADOR OR");
+console.log("a = 1");
+console.log("b = 1");
+console.log("c = 0");
+console.log("d = 0");
+console.log("a | b = ", a | b);
+console.log("c | d = ", c | d);
+//3.OPERADOR NOT
+console.log("3.OPERADOR NOT");
+console.log("b = 1");
+console.log("c = 0");
+console.log("~b = ", ~b);
+console.log("~c = ", ~c);
+//4.OPERADOR XOR
+console.log("4.OPERADOR XOR");
+console.log("a = 1");
+console.log("b = 1");
+console.log("c = 0");
+console.log("a ^ b = ", a ^ b);
+console.log("a ^ c = ", a ^ c);
