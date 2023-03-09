a09b0775b133af60ae0abbe65eb510a9c010a002
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-09 09:05:09 -0500

    feat: :sparkles: Add PI Math Method
    The Math method PI allows it use de value of PI (3,1416) for mathematical problems or operations.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-//METODOS 
+//METODOS MATH
+function calculaCircunferencia(radio) {
+    return 2 * Math.PI * radio;
+}
+console.log("Math.PI se utilizó para calcular la circunferencia de un circulo de radio (3):");
+console.log("Formula: (2 * Math.PI * radio) = ", calculaCircunferencia(3));
+console.log("Math.PI se utilizó para calcular la circunferencia de un circulo de radio (12):");
+console.log("Formula: (2 * Math.PI * radio) = ", calculaCircunferencia(12));
