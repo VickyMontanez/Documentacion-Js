ca3a78b379c74b04a590dd66ff6d50d476689566
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-09 08:47:58 -0500

    feat: :sparkles: Add Assignment Operators of JavaScript
    The Assignment operators  allows it assign values to variables and do basic mathematical operations

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-//OPERADORES LOGICOS
-//1.OPERADOR AND
-console.log("1.OPERADOR AND");
-let a = 1;
-let b = 1;
-let c = 0;
-let d = 0;
-console.log("a = 1");
-console.log("b = 1");
-console.log("c = 0");
-console.log("a & b = ", a & b);
-console.log("a & c = ", a & c);
-//2.OPERADOR OR
-console.log("2.OPERADOR OR");
-console.log("a = 1");
-console.log("b = 1");
-console.log("c = 0");
-console.log("d = 0");
-console.log("a | b = ", a | b);
-console.log("c | d = ", c | d);
-//3.OPERADOR NOT
-console.log("3.OPERADOR NOT");
-console.log("b = 1");
-console.log("c = 0");
-console.log("~b = ", ~b);
-console.log("~c = ", ~c);
-//4.OPERADOR XOR
-console.log("4.OPERADOR XOR");
-console.log("a = 1");
-console.log("b = 1");
-console.log("c = 0");
-console.log("a ^ b = ", a ^ b);
-console.log("a ^ c = ", a ^ c);
+//OPERADORES DE ASIGNACIÓN
+
+//1. OPERADOR DE ASIGNACIÓN
+console.log("1. OPERADOR DE ASIGNACIÓN");
+let a = 12;
+let b = 3;
+let c = a + b;
+console.log("a = ", a);
+console.log("b = ", b);
+//2. OPERADOR DE SUMA Y SIGNACIÓN
+console.log("2. OPERADOR DE SUMA Y ASIGNACIÓN");
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a += b : a = a + b : ", a+=b);
+//3. OPERADOR DE  RESTA Y ASIGNACIÓN
+console.log("3. OPERADOR DE RESTA Y ASIGNACIÓN");
+let x= 9;
+let y = 6;
+console.log("x = ", x);
+console.log("y = ", y);
+console.log("x -= y : x = x - y : ", x-=y);
+//4. OPERADOR DE  MULTIPLICACIÓN Y ASIGNACIÓN
+console.log("4. OPERADOR DE MULTIPLICACIÓN Y ASIGNACIÓN");
+let e= 12;
+let d = 4;
+console.log("e = ", e);
+console.log("d = ", d);
+console.log("e *= d : e = e * d : ", e*=d);
+//5. OPERADOR DE DIVISIÓN Y ASIGNACIÓN
+console.log("5. OPERADOR DE DIVISIÓN Y ASIGNACIÓN");
+let f= 34;
+let g = 7;
+console.log("f = ", f);
+console.log("g = ", g);
+console.log("f /= g : f = f / g : ", f/=g);
+//6. OPERADOR DE MODULO Y ASIGNACIÓN
+console.log("6. OPERADOR DE MODULO Y ASIGNACIÓN");
+let h = 50;
+let i= 10;
+console.log("h = ", h);
+console.log("i = ", i);
+console.log("h %= i : h = h % i : ", h%=i);
+//7. OPERADOR DE EXPONENCIACIÓN Y ASIGNACIÓN
+console.log("7. OPERADOR DE EXPONENCIACIÓN Y ASIGNACIÓN");
+let j= 3;
+let l= 10;
+console.log("j = ", j);
+console.log("l = ", l);
+console.log("j **= l : j = j ** l : ", j**=l);
