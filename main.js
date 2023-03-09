ad07286785c09547a1b5adad114a1c96214344ec
VickyMontanezCampus <vmontanez707@gmail.com>
2023-03-09 09:14:25 -0500

    feat: :sparkles: Add Abs Math Method
    The Math Method ABS allows it use the absolute value of a variable

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 //METODOS MATH
-function calculaCircunferencia(radio) {
-    return 2 * Math.PI * radio;
-}
-console.log("Math.PI se utilizó para calcular la circunferencia de un circulo de radio (3):");
-console.log("Formula: (2 * Math.PI * radio) = ", calculaCircunferencia(3));
-console.log("Math.PI se utilizó para calcular la circunferencia de un circulo de radio (12):");
-console.log("Formula: (2 * Math.PI * radio) = ", calculaCircunferencia(12));
+let a = -87654;
+let g = -78;
+let e = 764;
+let b = 43;
+console.log("Math.abs devuelve el valor absoluto de una variable");
+console.log("a =",a," Math.abs(a)=",Math.abs(a));
+console.log("b =",b," Math.abs(b)=",Math.abs(b));
+console.log("e =",e," Math.abs(e)=",Math.abs(e));
+console.log("g =",g," Math.abs(g)=",Math.abs(g));
