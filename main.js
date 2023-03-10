b58e45e61b2b1aa268499fe4848fb172783e27e8
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-09 22:44:55 -0500

    feat: :sparkles: Add Pow Math Method
    The Math Method POW returns the result of exponentiation by the base and the exponent of the operation

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
 //METODOS MATH
-let Array1 = [3435,657877,6653,989998,6565];
-let Array2 = [765745,868999.3546,9898, 4686749];
-let Array3 = [223566,7905654,7986,56677,89066];
-let x = 2365;
-let y = 35566; 
-let z = 7658;
+console.log("El método '.pow' me retorna el resultado de la exponeciación, a partir de la base y el exponente, ej:");
 
-console.log("El metodo '.min' me retorna el minimo número entre una lista o entre variables, ej:");
+console.log("EXPONENTES SIMPLES");
+console.log("Una potencia base '7' y exponente '2' : Math.pow(7, 2) =",Math.pow(7, 2));
+console.log("Una potencia base 2' y exponente '10' : Math.pow(2, 10) =",Math.pow(2, 10));
 
-console.log("EJEMPLOS ENTRE ARRAYS");
-console.log("Array1 =",Array1);
-console.log("El minimo valor del Array1 es 'Math.min(...Array1)' =",Math.min(...Array1));
-console.log("Array2 =",Array2);
-console.log("El minimo valor del Array2 es 'Math.min(...Array2)' =",Math.min(...Array2));
-console.log("Array3 =",Array3);
-console.log("El minimo valor del Array3 es 'Math.min(...Array3)' =",Math.min(...Array3));
+console.log("EXPONENTES FRACCIONALES");
+console.log("Una potencia base '2' y exponente '0.5' : Math.pow(2, 0.5) =",Math.pow(2, 0.5));
+console.log("Una potencia base '8' y exponente '1/3' : Math.pow(8, 1/3) =",Math.pow(8, 1/3));
 
-console.log("EJEMPLOS ENTRE VARIABLES");
-console.log("x =",x," y =", y," z =",z);
-console.log("El minimo valor entre las variables es 'Math.min(x,y,z)' =",Math.min(x,y,z));
+console.log("BASES CON SIGNO");
+console.log("Una potencia base '-7' y exponente '2' : Math.pow(-7, 2) =",Math.pow(-7, 2));
+console.log("Una potencia base '-2' y exponente '5' : Math.pow(-2, 5) =",Math.pow(-2, 5));    
