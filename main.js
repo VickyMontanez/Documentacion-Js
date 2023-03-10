206db16599a340334ccca2e35b1166ef9d7d712d
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-09 22:30:43 -0500

    feat: :sparkles: Add Min Math Method
    The Math Method MIN allows it find the min value of the array, list or variables.

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ let x = 2365;
 let y = 35566; 
 let z = 7658;
 
-console.log("El metodo '.max' me retorna el maxímo número entre una lista o entre variables, ej:");
+console.log("El metodo '.min' me retorna el minimo número entre una lista o entre variables, ej:");
 
 console.log("EJEMPLOS ENTRE ARRAYS");
 console.log("Array1 =",Array1);
-console.log("El maximo valor del Array1 es 'Math.max(...Array1)' =",Math.max(...Array1));
+console.log("El minimo valor del Array1 es 'Math.min(...Array1)' =",Math.min(...Array1));
 console.log("Array2 =",Array2);
-console.log("El maximo valor del Array2 es 'Math.max(...Array2)' =",Math.max(...Array2));
+console.log("El minimo valor del Array2 es 'Math.min(...Array2)' =",Math.min(...Array2));
 console.log("Array3 =",Array3);
-console.log("El maximo valor del Array3 es 'Math.max(...Array3)' =",Math.max(...Array3));
+console.log("El minimo valor del Array3 es 'Math.min(...Array3)' =",Math.min(...Array3));
 
 console.log("EJEMPLOS ENTRE VARIABLES");
 console.log("x =",x," y =", y," z =",z);
-console.log("El maximo valor entre las variables es 'Math.max(x,y,z)' =",Math.max(x,y,z));
+console.log("El minimo valor entre las variables es 'Math.min(x,y,z)' =",Math.min(x,y,z));
