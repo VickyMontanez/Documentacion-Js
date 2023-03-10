acc224a6183ce47970e0c245e3aed12a1516ec48
VickyMontanezCampus <125842395+VickyMontanezCampus@users.noreply.github.com>
2023-03-09 22:27:01 -0500

    feat: :sparkles: Add Max Math Method
    The Math Method MAX allows it find the max number of the array, list or variables.

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 //METODOS MATH
-let value1 = Math.log(1);
-let value2 = Math.log(10);
-let value3 = Math.log(8); 
+let Array1 = [3435,657877,6653,989998,6565];
+let Array2 = [765745,868999.3546,9898, 4686749];
+let Array3 = [223566,7905654,7986,56677,89066];
+let x = 2365;
+let y = 35566; 
+let z = 7658;
 
-console.log("Con el metodo 'Math.log' puedo encontrar el exponente de mi logaritmo base 'e' según el resultado, ej:");
-console.log("Encontrar el exponente de mi logaritmo base 'e', si su resultado es 1"); 
-console.log("El exponente de mi log 'e' es: Math.log(1) = ",value1,);
-console.log("Encontrar el exponente de mi logaritmo base 'e', si su resultado es 10"); 
-console.log("El exponente de mi log 'e' es: Math.log(10) = ",value2,);
-console.log("Encontrar el exponente de mi logaritmo base 'e', si su resultado es 8"); 
-console.log("El exponente de mi log 'e' es: Math.log(8) = ",value3,);
+console.log("El metodo '.max' me retorna el maxímo número entre una lista o entre variables, ej:");
+
+console.log("EJEMPLOS ENTRE ARRAYS");
+console.log("Array1 =",Array1);
+console.log("El maximo valor del Array1 es 'Math.max(...Array1)' =",Math.max(...Array1));
+console.log("Array2 =",Array2);
+console.log("El maximo valor del Array2 es 'Math.max(...Array2)' =",Math.max(...Array2));
+console.log("Array3 =",Array3);
+console.log("El maximo valor del Array3 es 'Math.max(...Array3)' =",Math.max(...Array3));
+
+console.log("EJEMPLOS ENTRE VARIABLES");
+console.log("x =",x," y =", y," z =",z);
+console.log("El maximo valor entre las variables es 'Math.max(x,y,z)' =",Math.max(x,y,z));
