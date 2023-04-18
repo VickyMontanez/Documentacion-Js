fc912f4fa20808cc0ed90b69ccf9983bc92cbb98
VickyMontanezCampus <vmontanez707@gmail.com>
2023-04-18 08:50:12 -0500

    feat: :rocket: Config Workspace
    Config the Workspace for new documentation about functions Js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
-//METODOS MATH
-console.log("El método '.pow' me retorna el resultado de la exponeciación, a partir de la base y el exponente, ej:");
-
-console.log("EXPONENTES SIMPLES");
-console.log("Una potencia base '7' y exponente '2' : Math.pow(7, 2) =",Math.pow(7, 2));
-console.log("Una potencia base 2' y exponente '10' : Math.pow(2, 10) =",Math.pow(2, 10));
-
-console.log("EXPONENTES FRACCIONALES");
-console.log("Una potencia base '2' y exponente '0.5' : Math.pow(2, 0.5) =",Math.pow(2, 0.5));
-console.log("Una potencia base '8' y exponente '1/3' : Math.pow(8, 1/3) =",Math.pow(8, 1/3));
-
-console.log("BASES CON SIGNO");
-console.log("Una potencia base '-7' y exponente '2' : Math.pow(-7, 2) =",Math.pow(-7, 2));
-console.log("Una potencia base '-2' y exponente '5' : Math.pow(-2, 5) =",Math.pow(-2, 5));    
+// FUNCIONES JS
